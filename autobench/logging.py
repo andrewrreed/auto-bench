2d5f767342c38b75bc91b0ca05e562641eed5d4f
Andrew Reed <andrew.reed.r@gmail.com>
2024-09-20 14:02:18 -0400

    ignore logger in notebook

--- autobench/logging.py
+++ autobench/logging.py
@@ -3,14 +3,15 @@ import sys
 
 
 def setup_logging():
-    logger.remove()
 
+    LOG_LEVEL = "WARNING" if "ipykernel" in sys.modules else "INFO"
+
+    logger.remove()
     logger.add(
         sys.stderr,
         format="<green>{time:YYYY-MM-DD HH:mm:ss}</green> | <level>{level: <8}</level> | <cyan>{name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan> - <level>{message}</level>",
-        level="INFO",
+        level=LOG_LEVEL,
     )
-
     logger.add(
         "./logs/autobench.log", rotation="10 MB", retention="1 week", level="DEBUG"
     )
